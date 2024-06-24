c3eddfc46389bae3680b8f78e615775f50ef031f
kwanghwi <nimo6289@gmail.com>
2024-06-24 15:16:49 +0900

    백엔드 init

---
+++ BackEnd/src/main/java/DGU/CECD/VTF/VtfApplication.java
@@ -0,0 +1,13 @@
+package DGU.CECD.VTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtfApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/DGU/CECD/VTF/VtfApplicationTests.java
@@ -0,0 +1,13 @@
+package DGU.CECD.VTF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
